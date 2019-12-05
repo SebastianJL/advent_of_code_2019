3b50425633b3056297e8f97beecd0dc9cefaaeb3
SebastianJL <johannes.lade@yahoo.com>
2019-12-05 11:05:43 +0100

    day 2

---
+++ day2/day2_ex1_ex2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2019/12/05
+
+@author: johannes
+"""
+from typing import List
+
+
+class IntcodeComputer:
+    # 1 = add
+    # 2 = multiply
+
+    def __init__(self, intcode: List[int]):
+
+        self.tape = intcode
+
+    def run(self):
+        for instruction in self.instructions():
+            opcode, n1, n2, mem = instruction
+
+            if opcode == 1:
+                self.tape[mem] = self.tape[n1] + self.tape[n2]
+            elif opcode == 2:
+                self.tape[mem] = self.tape[n1] * self.tape[n2]
+        return self.tape
+
+    def instructions(self):
+        i = 0
+        while True:
+            if self.tape[i] == 99:
+                raise StopIteration
+            else:
+                yield self.tape[i:i + 4]
+                i += 4
+
+
+if __name__ == '__main__':
+    # Ex1
+    # intcode = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,9,19,23,1,9,23,27,2,27,9,31,1,31,5,35,2,35,9,39,1,39,10,43,2,43,13,47,1,47,6,51,2,51,10,55,1,9,55,59,2,6,59,63,1,63,6,67,1,67,10,71,1,71,10,75,2,9,75,79,1,5,79,83,2,9,83,87,1,87,9,91,2,91,13,95,1,95,9,99,1,99,6,103,2,103,6,107,1,107,5,111,1,13,111,115,2,115,6,119,1,119,5,123,1,2,123,127,1,6,127,0,99,2,14,0,0]
+    # intcode[1] = 12
+    # intcode[2] = 2
+    # print(intcode)
+    # icc = IntcodeComputer(intcode)
+    # print(icc.run())
+
+    # Ex2
+    for noun in range(137):
+        for verb in range(136, -1, -1):
+            intcode = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,9,19,23,1,9,23,27,2,27,9,31,1,31,5,35,2,35,9,39,1,39,10,43,2,43,13,47,1,47,6,51,2,51,10,55,1,9,55,59,2,6,59,63,1,63,6,67,1,67,10,71,1,71,10,75,2,9,75,79,1,5,79,83,2,9,83,87,1,87,9,91,2,91,13,95,1,95,9,99,1,99,6,103,2,103,6,107,1,107,5,111,1,13,111,115,2,115,6,119,1,119,5,123,1,2,123,127,1,6,127,0,99,2,14,0,0]
+            intcode[1] = noun
+            intcode[2] = verb
+            icc = IntcodeComputer(intcode)
+            result = icc.run()
+            if result[0] == 19690720:
+                print(noun, verb)
+                print(100*noun + verb)
+                print(result)
+                exit()
